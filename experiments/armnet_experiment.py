c6ba54b767ca882256750e6fceb619ce83bb8bbd
Danilo Calhes <dancps96@gmail.com>
2025-04-21 14:23:02 -0300

    Includes K-Fold experiment

--- experiments/armnet_experiment.py
+++ experiments/armnet_experiment.py
@@ -4,24 +4,63 @@ from masters.models.armnet import RMFNet
 from tensorflow.keras.applications import ResNet50
 import argparse
 
+
 class ArmnetExperiment(Experiment):
     """
-        ArmnetExperiment is a subclass of the Experiment class.
-        It is used to run the Armnet experiment.
+    ArmnetExperiment is a subclass of the Experiment class.
+    It is used to run the Armnet experiment.
     """
+
     def __init__(self, development=False):
-        dataset = Dataset("data/datasets/mbtd/raw", (224, 224), 0.2, 64, seed=123)
+        dataset = Dataset(
+            dataset_name="brain_tumor_mri_dataset_kaggle",
+            input_shape=(224, 224),
+            folds=1,
+            validation_split_size=0.2,
+            batch_size=64,
+            seed=123,
+        )
         epochs = 50
         model = RMFNet()
         experiment_name = "armnet_experiment"
         super().__init__(experiment_name, model, epochs, dataset)
 
+
+class ArmnetKFoldExperiment(Experiment):
+    """
+    x
+    """
+
+    def __init__(self, development=False):
+        folds = 10
+        dataset = Dataset(
+            dataset_name="brain_tumor_mri_dataset_kaggle",
+            input_shape=(224, 224),
+            folds=folds,
+            validation_split_size=0.2,
+            batch_size=64,
+            seed=123,
+        )
+        epochs = 50
+        model = RMFNet()
+        experiment_name = "armnet_experiment"
+        super().__init__(experiment_name, model, epochs, dataset, folds)
+
+
 class ResNetExperiment(Experiment):
     """
-        x
+    x
     """
+
     def __init__(self, development=False):
-        dataset = Dataset("data/datasets/mbtd/raw", (224, 224), 0.2, 64, seed=123)
+        dataset = Dataset(
+            "brain_tumor_mri_dataset_kaggle",
+            input_shape=(224, 224),
+            folds=1,
+            validation_split_size=0.2,
+            batch_size=64,
+            seed=123,
+        )
         epochs = 50
         model = ResNet50(
             include_top=True,
@@ -30,20 +69,30 @@ class ResNetExperiment(Experiment):
             input_shape=None,
             pooling=None,
             classes=4,
-            classifier_activation='softmax'
+            classifier_activation="softmax",
         )
         experiment_name = "resnet_experiment"
         super().__init__(experiment_name, model, epochs, dataset)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--experiments', nargs='+', default = [], help='which experiments should run.',)
-    parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
-    parser.add_argument('-d', '--development', help='changes to dev experience', action='store_true')
+    parser.add_argument(
+        "-e",
+        "--experiments",
+        nargs="+",
+        default=[],
+        help="which experiments should run.",
+    )
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument(
+        "-d", "--development", help="changes to dev experience", action="store_true"
+    )
     args = parser.parse_args()
 
     all_experiments = [
         ArmnetExperiment(),
+        ArmnetKFoldExperiment(),
         ResNetExperiment(),
     ]
     if args.experiments == []:
