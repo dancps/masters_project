33d9f1d6783a9488b241751cbb2da0dfc5cfa3ed
Danilo C. <>
2025-04-21 19:18:37 -0300

    Format updates

--- op/experiment.py
+++ op/experiment.py
@@ -7,35 +7,44 @@ import datetime
 from tabulate import tabulate
 from termcolor import colored
 
+
 class Experiment:
     """
-        A experiment should recieve everything which is important to a run.
+    A experiment should recieve everything which is important to a run.
 
-        The idea is to standardize the experiment, so you can either run a prod or dev run:
-            python experiment.py run
-        or 
-            python experiment.py run-development 
+    The idea is to standardize the experiment, so you can either run a prod or dev run:
+        python experiment.py run
+    or
+        python experiment.py run-development
 
-        It should: 
-            - be able to run the steps of the experiment.
-            - be able to save the results of the experiment.
+    It should:
+        - be able to run the steps of the experiment.
+        - be able to save the results of the experiment.
     """
-    def __init__(self, experiment_name: str, model: Model, epochs: int = None,  dataset: Dataset = None, folds:int = 1):
+
+    def __init__(
+        self,
+        experiment_name: str,
+        model: Model,
+        epochs: int = None,
+        dataset: Dataset = None,
+        folds: int = 1,
+    ):
         self.experiment_name = experiment_name
         self.epochs = epochs
         self.dataset = dataset
         self.model = model
-        self.folds = folds # TODO: Currently, folds is defined both in dataset and at here. I should review it.
-        self.is_k_fold = folds>1
+        self.folds = folds  # TODO: Currently, folds is defined both in dataset and at here. I should review it.
+        self.is_k_fold = folds > 1
         # self.metrics = metrics
 
     # def train(self):
     #     raise NotImplementedError("Train method are not implemented yet.")
-    
+
     # def test(self):
     #     raise NotImplementedError("Evaluate method are not implemented yet.")
 
-    def train(self, is_development = False):
+    def train(self, is_development=False):
 
         if self.is_k_fold:
             # Load data
@@ -47,7 +56,7 @@ class Experiment:
             val_ds = [self.dataset.get_validation_dataset(is_development)]
 
         for fold in range(self.folds):
-            print(colored(f"Fold {fold+1}/{self.folds}", 'cyan'))
+            print(colored(f"Fold {fold+1}/{self.folds}", "cyan"))
 
             # Used to measure performance and to identify the run
             start_time = datetime.datetime.now()
@@ -65,13 +74,13 @@ class Experiment:
             if is_development:
                 epochs = 3
                 stage = "dev"
-            else: 
+            else:
                 epochs = self.epochs
                 stage = "prod"
 
-            # Defines the metrics 
-            f1 = F1Score(average='macro', threshold=0.5)
-            precision_metric = tf.keras.metrics.Precision(name = 'precision')#, class_id = 4)
+            # Defines the metrics
+            f1 = F1Score(average="macro", threshold=0.5)
+            precision_metric = tf.keras.metrics.Precision(name="precision")  # , class_id = 4)
 
             # Defines the checkpoints
             #   If not exists, creates the directory
@@ -79,38 +88,44 @@ class Experiment:
             if self.is_k_fold:
                 k_fold_identifier = f"fold-{fold+1}"
                 k_fold_dir = f"/{k_fold_identifier}"
-            
+
             checkpoints_path = f"data/experiments/{self.experiment_name}/checkpoints/{stage}{k_fold_dir}/{start_time_identifier}"
             model_checkpoint = f"{checkpoints_path}/model"
 
             # Saves all the epochs
             all_epochs_callback = tf.keras.callbacks.ModelCheckpoint(
-                filepath=f"{model_checkpoint}/all_epochs/{self.experiment_name}"+"-{epoch:04d}.weights.h5",
+                filepath=f"{model_checkpoint}/all_epochs/{self.experiment_name}"
+                + "-{epoch:04d}.weights.h5",
                 save_weights_only=True,
-                verbose=1)
+                verbose=1,
+            )
 
             # Saves the best only
             best_train_callback = tf.keras.callbacks.ModelCheckpoint(
-                filepath=f"{model_checkpoint}/{self.experiment_name}-best"+"-{epoch:04d}.weights.h5",
+                filepath=f"{model_checkpoint}/{self.experiment_name}-best"
+                + "-{epoch:04d}.weights.h5",
                 save_weights_only=True,
                 save_best_only=True,
                 monitor=f1.name,
-                verbose=1)
+                verbose=1,
+            )
 
             # Saves the best val only
             best_val_callback = tf.keras.callbacks.ModelCheckpoint(
-                filepath=f"{model_checkpoint}/{self.experiment_name}-best-val"+"-{epoch:04d}.weights.h5",
+                filepath=f"{model_checkpoint}/{self.experiment_name}-best-val"
+                + "-{epoch:04d}.weights.h5",
                 save_weights_only=True,
                 save_best_only=True,
-                monitor='val_'+f1.name,
-                verbose=1)
+                monitor="val_" + f1.name,
+                verbose=1,
+            )
 
             # Saves the last one
             last_callback = tf.keras.callbacks.ModelCheckpoint(
                 filepath=f"{model_checkpoint}/{self.experiment_name}-last.weights.h5",
                 save_weights_only=True,
-                verbose=1)
-
+                verbose=1,
+            )
 
             # For using at tensorboard
             log_checkpoint = f"{checkpoints_path}/logs/"
@@ -120,30 +135,31 @@ class Experiment:
                 write_graph=True,
                 write_images=True,
                 write_steps_per_second=False,
-                update_freq='epoch',
+                update_freq="epoch",
                 profile_batch=0,
                 embeddings_freq=0,
-                embeddings_metadata=None)
+                embeddings_metadata=None,
+            )
 
             self.model.compile(
-                optimizer='adam',
+                optimizer="adam",
                 # loss='categorical',
-                loss='categorical_crossentropy',
+                loss="categorical_crossentropy",
                 # loss=tf.losses.SparseCategoricalCrossentropy(from_logits=False), #'categorical_crossentropy', #
-                metrics=['accuracy', precision_metric, f1]
-            )# F1Score()
-            
+                metrics=["accuracy", precision_metric, f1],
+            )  # F1Score()
+
             history = self.model.fit(
                 current_train_ds,
                 validation_data=current_val_ds,
-                epochs=epochs, 
+                epochs=epochs,
                 callbacks=[
                     all_epochs_callback,
                     best_train_callback,
                     best_val_callback,
                     last_callback,
-                    tensorboard_callback
-                ]
+                    tensorboard_callback,
+                ],
             )
 
             print(history)
@@ -156,21 +172,21 @@ class Experiment:
                         ["Start time", start_time],
                         ["End time", end_time],
                         ["Duration", end_time - start_time],
-                        ["Identifier", start_time_identifier]
+                        ["Identifier", start_time_identifier],
                     ],
                     tablefmt="fancy_grid",
-                    )
                 )
+            )
 
-    def evaluate(self, is_development = False):
+    def evaluate(self, is_development=False):
 
         test_ds = self.dataset.get_test_dataset(is_development)
 
         print("Evaluate")
         result = self.model.evaluate(test_ds)
         print(dict(zip(self.model.metrics_names, result)))
-        
-    def run(self, is_development = False):
+
+    def run(self, is_development=False):
         # Run the steps of the experiment
         #     What composes?
 
@@ -182,11 +198,6 @@ class Experiment:
         # - Train
         # - Test
         # - Results
-        print(f"Running the experiment: "+ colored(self.experiment_name, 'green'))
+        print(f"Running the experiment: " + colored(self.experiment_name, "green"))
         self.train(is_development)
         self.evaluate(is_development)
-
-
-        
-        
-
