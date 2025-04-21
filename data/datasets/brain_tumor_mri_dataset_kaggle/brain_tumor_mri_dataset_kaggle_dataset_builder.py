ae1316fcfe6194bf10b123ea45fd309e6a396ed5
Danilo Calhes <dancps96@gmail.com>
2025-04-21 11:59:00 -0300

    Updates(before clean-up of playground/testing_datasets_cross_validation.py)

--- data/datasets/brain_tumor_mri_dataset_kaggle/brain_tumor_mri_dataset_kaggle_dataset_builder.py
+++ data/datasets/brain_tumor_mri_dataset_kaggle/brain_tumor_mri_dataset_kaggle_dataset_builder.py
@@ -7,61 +7,90 @@ Read this to learn more:
 
 import tensorflow_datasets as tfds
 import os
+from termcolor import colored
+
+
+CLASSES = ["glioma", "meningioma", "notumor", "pituitary"]
+
+
+def info(label, text):
+    print(f"[{colored(label, 'blue')}] {colored(text, 'green')}")
+
 
 class Builder(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for brain_tumor_mri_dataset_kaggle dataset."""
+    """DatasetBuilder for brain_tumor_mri_dataset_kaggle dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    # TODO(brain_tumor_mri_dataset_kaggle): Specifies the tfds.core.DatasetInfo object
-    return self.dataset_info_from_configs(
-        features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
-            'image': tfds.features.Image(), # can use this argument to specify the image size shape=(None, None, 3)
-            'label': tfds.features.ClassLabel(names=['glioma', 'meningioma','notumor','pituitary']),
-        }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('image', 'label'),  # Set to `None` to disable
-        homepage='https://www.kaggle.com/datasets/masoudnickparvar/brain-tumor-mri-dataset',
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    # TODO(brain_tumor_mri_dataset_kaggle): Downloads the data and defines the splits
-    path = dl_manager.download_and_extract('https://www.kaggle.com/api/v1/datasets/download/masoudnickparvar/brain-tumor-mri-dataset')
-
-    # TODO(brain_tumor_mri_dataset_kaggle): Returns the Dict[split names, Iterator[Key, Example]]
-    return {
-        'train': self._generate_examples(path / 'Training'),
-        'test': self._generate_examples(path / 'Testing'),
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release.",
     }
 
-  def _generate_examples(self, filepath):
-    """Yields examples."""
-    # TODO(brain_tumor_mri_dataset_kaggle): Yields (key, example) tuples from the dataset
-    # for f in path.glob('*.jpg'):
-    #   yield 'key', {
-    #       'image': f,
-    #       'label': 'yes',
-    #   }
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        # TODO(brain_tumor_mri_dataset_kaggle): Specifies the tfds.core.DatasetInfo object
+        return self.dataset_info_from_configs(
+            features=tfds.features.FeaturesDict(
+                {
+                    # These are the features of your dataset like images, labels ...
+                    "image": tfds.features.Image(),  # can use this argument to specify the image size shape=(None, None, 3)
+                    "label": tfds.features.ClassLabel(names=CLASSES),
+                }
+            ),
+            # If there's a common (input, target) tuple from the
+            # features, specify them here. They'll be used if
+            # `as_supervised=True` in `builder.as_dataset`.
+            supervised_keys=("image", "label"),  # Set to `None` to disable
+            homepage="https://www.kaggle.com/datasets/masoudnickparvar/brain-tumor-mri-dataset",
+        )
 
-    """Yields image examples from folder structure."""
-    for class_name in sorted(os.listdir(filepath)):
-        class_dir = os.path.join(filepath, class_name)
-        if not os.path.isdir(class_dir):
-            continue  # skip non-directory files, if any
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        # TODO(brain_tumor_mri_dataset_kaggle): Downloads the data and defines the splits
+        path = dl_manager.download_and_extract(
+            "https://www.kaggle.com/api/v1/datasets/download/masoudnickparvar/brain-tumor-mri-dataset"
+        )
 
-        for img_name in os.listdir(class_dir):
-            img_path = os.path.join(class_dir, img_name)
-            key = f"{class_name}/{img_name}"
-            yield key, {
-                "image": img_path,
-                "label": class_name,
-            }
+        # TODO(brain_tumor_mri_dataset_kaggle): Returns the Dict[split names, Iterator[Key, Example]]
+        # return {
+        #     "train": self._generate_examples(path / "Training"),
+        #     "test": self._generate_examples(path / "Testing"),
+        # }
+        info("Path", f"{path}")  # For debugging
+        info("Path", f"{tfds.Split.TRAIN}")  # For debugging
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                gen_kwargs={
+                    "filepath": os.path.join(path, "Training"),
+                },
+            ),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TEST,
+                gen_kwargs={
+                    "filepath": os.path.join(path, "Testing"),
+                },
+            ),
+        ]
+
+    # TODO: Not used yet(?)
+    def _generate_examples(self, filepath):
+        """Yields examples."""
+        info("Path", f"{filepath}")  # For debugging
+        for class_name in sorted(CLASSES):
+            class_dir = os.path.join(filepath, class_name)
+
+            # Maybe the validation below shoudlnt continue. Should be a hard validation, failing on inconsistencies
+            if not os.path.isdir(class_dir):
+                continue  # skip non-directory files, if any
+
+            count = 0  # For debugging
+            for img_name in os.listdir(class_dir):
+                count += 1
+                img_path = os.path.join(class_dir, img_name)
+                key = f"{class_name}/{img_name}"
+                yield key, {
+                    "image": img_path,
+                    "label": class_name,
+                }
+
+            info(f"{class_name}", f"Found {count} images")  # For debugging
--- playground/testing_datasets_cross_validation.py
+++ playground/testing_datasets_cross_validation.py
@@ -1,8 +1,12 @@
 import masters.data.datasets.brain_tumor_mri_dataset_kaggle
 import tensorflow_datasets as tfds
+from tabulate import tabulate
+from termcolor import colored
+
 
 # Read: 
 #   - https://www.tensorflow.org/datasets/splits#cross_validation
+#   - https://www.tensorflow.org/datasets/splits#tfdseven_splits_multi-host_training (even splits, for a diff k-fold)
 
 
 # dataset = Dataset("data/datasets/mbtd/raw", (224, 224), 0.2, 64, seed=123)
@@ -18,23 +22,89 @@ import tensorflow_datasets as tfds
 # #     f'train[:{k}%]+train[{k+10}%:]' for k in range(0, 100, 10)
 # # ])
 
+def check_classes_distribution(ds):
+    # print(f"Split: {type(ds)}")
+    print("Class distribution:")
+
+    e = ds.take(1)
+
+    print(f"DS  : {e}")
+    print([x for x in dir(e) if not x.startswith('_')])
+    for ex in e:
+        print(f"Example size: {len(ex)}")
+
+        # img = ex['image']
+        # label = ex['label']
+        # print(f"Image shape: {img.shape}, Label: {label}")
 
 # tds = dataset.get_train_dataset()
 
 # print(tds['0:100'])
 dataset_name = 'brain_tumor_mri_dataset_kaggle'
+# dataset_name = 'omniglot'
 
-ds = tfds.load(dataset_name, split='train', shuffle_files=True)
+K = 10 # number of folds
+train_split_array = []
+validation_split_array = []
+for k in [k for k in range(0, 100, K)]:
+    train_split_str = f"train[:{k}%]+train[{k+10}%:]"
+    val_split_str = f"train[{k}%:{k+10}%]"
+
+    train_split_array.append(train_split_str)
+    validation_split_array.append(val_split_str)
+    # print(f"Validation: train[{k}%:{k+10}%]")
+    # print(f"Train     : ")
+    # print()
+
+ds_train = tfds.load(dataset_name, split=train_split_array, as_supervised=True)
+ds_val   = tfds.load(dataset_name, split=validation_split_array, as_supervised=True)
+ds_test  = tfds.load(dataset_name, split="test", as_supervised=True)
 
 
-ds_train = tfds.load(
-    dataset_name, 
-    split=[
-        f'train[:{k}%]+train[{k+10}%:]' for k in range(0, 100, 10)
-    ], 
-    # as_supervised=True
+ds = ds_train[0]
+ds = ds.take(1)
+
+for image, label in ds:  # example is (image, label)
+    print(image.shape, label)
+
+
+print(colored(f"Type of ds_train: {type(ds_train)}", "cyan"))
+print(colored(f"Type of first item in ds_train: {type(ds_train[0])}", "blue"))
+print(colored(f"Type of first element in ds_train[0]: {type(list(ds_train[0].as_numpy_iterator())[0])}", "green"))
+print(
+    tabulate(
+        [
+            ["", "Size of set", "Equal to K", "Type"],
+            ["Number of folds(val)", len(ds_val), len(ds_val)==K, type(ds_val[0])],
+            ["Number of folds(train)", len(ds_train), len(ds_train)==K, "list of "+str(type(ds_train[0]))],
+            ["Number of folds(test)", len(ds_test),"", type(ds_test)],
+        ],
+        tablefmt="fancy_grid",
+        )
     )
 
-ds_val = tfds.load(dataset_name, as_supervised=True)
-print(len(ds_train))
-print(ds_train)
+ds_test = ds_test.take(1)
+for test_example in ds_test:
+    # print(f"Test example: {test_example}")
+    try:
+        print(test_example["label"])
+    except:
+        print(colored("test_example['label'] not available", "red"))
+        pass
+
+
+    
+image, label = list(ds_test)[0]
+print(f"Image shape: {image.shape}, Label: {label}")
+
+
+# Check for each fold class distribution
+for split_idx, split in enumerate(ds_train):
+    print(f"Fold {split_idx+1}/{len(ds_train)} train split")
+    print(f"Split type: {type(split)}")
+    print(f"Split length: {len(split)}")
+    
+    print(split["image"])
+    print(list(split.keys()))
+    check_classes_distribution(split)
+    print()
