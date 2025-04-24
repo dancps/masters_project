a9d8a0ddf71feb96ae6e8d072c496dbeb2be2bc4
Danilo C. <>
2025-04-24 00:51:45 -0300

    Updates experiments

--- experiments/armnet_experiment.py
+++ experiments/armnet_experiment.py
@@ -46,26 +46,45 @@ class Armnet10FoldExperiment(Experiment):
         experiment_name = "armnet_10_folds_experiment"
         super().__init__(experiment_name, model, epochs, dataset, folds)
 
-
-class Armnet5FoldExperiment(Experiment):
+class Armnet128Experiment(Experiment):
     """
-    x
+    ArmnetExperiment is a subclass of the Experiment class.
+    It is used to run the Armnet experiment.
     """
 
     def __init__(self, development=False):
-        folds = 5
         dataset = Dataset(
             dataset_name="brain_tumor_mri_dataset_kaggle",
             input_shape=(224, 224),
-            folds=folds,
+            folds=10,
             validation_split_size=0.2,
-            batch_size=64,
+            batch_size=128,
             seed=123,
         )
         epochs = 50
         model = RMFNet()
-        experiment_name = "armnet_5_folds_experiment"
-        super().__init__(experiment_name, model, epochs, dataset, folds)
+        experiment_name = "armnet_bs128_experiment"
+        super().__init__(experiment_name, model, epochs, dataset)
+
+# class Armnet5FoldExperiment(Experiment):
+#     """
+#     x
+#     """
+
+#     def __init__(self, development=False):
+#         folds = 5
+#         dataset = Dataset(
+#             dataset_name="brain_tumor_mri_dataset_kaggle",
+#             input_shape=(224, 224),
+#             folds=folds,
+#             validation_split_size=0.2,
+#             batch_size=64,
+#             seed=123,
+#         )
+#         epochs = 50
+#         model = RMFNet()
+#         experiment_name = "armnet_5_folds_experiment"
+#         super().__init__(experiment_name, model, epochs, dataset, folds)
 
 
 class ResNetExperiment(Experiment):
@@ -122,32 +141,32 @@ class ResNet10FoldsExperiment(Experiment):
         experiment_name = "resnet_10_folds_experiment"
         super().__init__(experiment_name, model, epochs, dataset)
 
-class ResNet5FoldsExperiment(Experiment):
-    """
-    x
-    """
+# class ResNet5FoldsExperiment(Experiment):
+#     """
+#     x
+#     """
 
-    def __init__(self, development=False):
-        dataset = Dataset(
-            "brain_tumor_mri_dataset_kaggle",
-            input_shape=(224, 224),
-            folds=5,
-            validation_split_size=0.2,
-            batch_size=64,
-            seed=123,
-        )
-        epochs = 50
-        model = ResNet50(
-            include_top=True,
-            weights=None,
-            input_tensor=None,
-            input_shape=None,
-            pooling=None,
-            classes=4,
-            classifier_activation="softmax",
-        )
-        experiment_name = "resnet_5_folds_experiment"
-        super().__init__(experiment_name, model, epochs, dataset)
+#     def __init__(self, development=False):
+#         dataset = Dataset(
+#             "brain_tumor_mri_dataset_kaggle",
+#             input_shape=(224, 224),
+#             folds=5,
+#             validation_split_size=0.2,
+#             batch_size=64,
+#             seed=123,
+#         )
+#         epochs = 50
+#         model = ResNet50(
+#             include_top=True,
+#             weights=None,
+#             input_tensor=None,
+#             input_shape=None,
+#             pooling=None,
+#             classes=4,
+#             classifier_activation="softmax",
+#         )
+#         experiment_name = "resnet_5_folds_experiment"
+#         super().__init__(experiment_name, model, epochs, dataset)
 
 
 if __name__ == "__main__":
@@ -168,10 +187,9 @@ if __name__ == "__main__":
     all_experiments = [
         ArmnetExperiment(),
         Armnet10FoldExperiment(),
-        Armnet5FoldExperiment(),
+        Armnet128Experiment(),
         ResNetExperiment(),
-        ResNet10FoldsExperiment(),
-        ResNet5FoldsExperiment()
+        ResNet10FoldsExperiment()
     ]
     
     if args.experiments == []:
